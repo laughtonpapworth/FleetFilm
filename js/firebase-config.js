8c6e3dcef0b98f04c67497e08b9eedbaef114612
laughtonpapworth <97966584+laughtonpapworth@users.noreply.github.com>
2025-10-03 11:40:26 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,16 +2,16 @@
 
 // 1) Your Firebase config (fill in your real values)
 window.__FLEETFILM__CONFIG = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyDb3RwlWUutBwBg8BGwAPkMG0Iv1_of5Mg",
   authDomain: "fleetfilm-8829e.firebaseapp.com",
   projectId: "fleetfilm-8829e",
   storageBucket: "fleetfilm-8829e.appspot.com",
   messagingSenderId: "464150585740",
-  appId: "YOUR_APP_ID",
+  appId: "1:464150585740:web:a047ae9b57ce149bea76a9",
 
   // Optional keys used by your app:
-  omdbApiKey: "",        // if you have one
-  getaddressIoKey: ""    // if you have one
+  omdbApiKey: "3e90e2e2",        // if you have one
+  getaddressIoKey: "Ll_nRO8O302rbH7q5roJzA47908"    // if you have one
 };
 
 // 2) Hard-init Firebase immediately so redirect completion can run on mobile
